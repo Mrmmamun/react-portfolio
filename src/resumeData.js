36c97c09ccf1d0a9c42ee674ea1d695a94e8943d
Mamunur Rashid <43251853+Mrmmamun@users.noreply.github.com>
2020-06-02 16:13:42 +0600

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Mamunur Rashid",
+    "role": "Web Designer Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+    "address":"Bangladesh",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/console.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/origami.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/into-the-light.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
